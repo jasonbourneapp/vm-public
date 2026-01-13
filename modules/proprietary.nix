40fa95ef3f2d42704a0598a03960876759245eae
jasonbourne <jasonbourne@example.com>
2026-01-13 18:15:01 +0300

    try build ffmpeg-headless with libpulse

--- modules/proprietary.nix
+++ modules/proprietary.nix
@@ -14,6 +14,7 @@ let
   gnomeShellStorePath = builtins.getEnv gnomeShellEnvVar;
   jasonbourneStorePath = builtins.getEnv jasonbourneEnvVar;
 
+
   mkProprietary = originalPkg: storePath: envVarName:
     # Check if path is empty to provide a helpful error message
     if storePath == "" then
@@ -44,6 +45,11 @@ let
   proprietaryOverlay = final: prev: {
     mutter       = mkProprietary prev.mutter mutterStorePath mutterEnvVar;
     gnome-shell = mkProprietary prev.gnome-shell gnomeShellStorePath gnomeShellEnvVar;
+
+    ffmpeg-headless = prev.ffmpeg-headless.overrideAttrs (old: {
+      buildInputs = (old.buildInputs or []) ++ [ prev.pulseaudio ];
+      configureFlags = (old.configureFlags or []) ++ [ "--enable-libpulse" ];
+    });
 
     jasonbourne = if jasonbourneStorePath == "" then
       throw "${jasonbourneEnvVar} env var is empty."
